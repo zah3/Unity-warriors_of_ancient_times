8297f25964ee120ddbb94ed9e18a87672ffccc29
Zachariasz <zah3@o2.pl>
2017-05-28 11:06:43 +0200

    update project

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	static MusicPlayer instance = null;
+	
+	void Awake(){
+
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+	
+	}
+}
